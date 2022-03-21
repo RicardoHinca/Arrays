06c982f3c68e4d5a72a3f304ac660c7c6c3d5eec
RicardoHinca <rhinca@icloud.com>
2022-03-21 17:42:11 -0300

    Equals como usar e conceito

---
+++ equalseHashcode/Equals.java
@@ -0,0 +1,27 @@
+package arrays.equalseHashcode;
+
+import java.util.Date;
+
+public class Equals {
+
+	public static void main(String[] args) {
+		
+		Usuarios u1 = new Usuarios();
+		u1.nome = "Pedro Silva";
+		u1.email = "pedro.silva@ezemail.com.br";
+		
+		Usuarios u2 = new Usuarios();
+		u2.nome = "Pedro Silva";
+		u2.email = "pedro.silva@ezemail.com.br";
+		
+		System.out.println(u1 == u2);
+		System.out.println(u1.equals(u2));
+		System.out.println(u2.equals(u1));
+		
+		System.out.println(u2.equals(new Date()));
+		
+//		System.out.println(u1== u1);
+//		System.out.println(u1.equals(u1));
+		
+	}
+}
---
+++ equalseHashcode/Usuarios.java
@@ -0,0 +1,22 @@
+package arrays.equalseHashcode;
+
+public class Usuarios {
+
+	String nome;
+	String email;
+	
+	public boolean equals(Object objeto) {
+		
+	if (objeto instanceof Usuarios) {
+			
+		Usuarios outro = (Usuarios) objeto;
+		
+		boolean nomeIgual = outro.nome.equals(this.nome); 
+		boolean emailIgual = outro.email.equals(this.email);
+		
+		return nomeIgual && emailIgual;
+	} else {
+			return false; 
+		}
+	}
+}
